0c5c04faa31203116df14613a8a297a1113795d8
Tailene Gomes <gtailene@gmail.com>
2023-11-22 16:21:50 -0300

    Término do trabalho Gerador de QR Code

--- code.js
+++ code.js
@@ -1,8 +1,21 @@
-const formulario = document.getElementById("formulario")
-const inputText = document.getElementById("inputText")
-const img = document.getElementById("imagemQRCode")
+const container = document.querySelector('.container'),
+  Input = document.querySelector('.form input'),
+  btn = container.querySelector('.form button'),
+  Img = container.querySelector('.qr-code img');
 
-formulario.addEventListener("submit",function(event){
-  event.preventDefault()
-  img.src = "http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + imputText.value
+btn.addEventListener('click', () => {
+  let qrValue = Input.value;
+  if (!qrValue) return;
+  btn.innerText = "Gerador QR Code"
+  Img.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  Img.addEventListener('load', () => {
+    container.classList.add('active');
+    btn.innerText = "Gerar QR Code"
+  })
+});
+
+Input.addEventListener('keyup', () =>{
+  if(!Input.value){
+    container.classList.remove('active');
+  }
 })
